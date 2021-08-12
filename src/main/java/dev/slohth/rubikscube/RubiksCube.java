14bb81656d46f1e4af09c117b590e15206b115bb
Slohth <hellspawnp@gmail.com>
2021-08-12 16:50:06 +0100

    Removed redundant code

--- src/main/java/dev/slohth/rubikscube/RubiksCube.java
+++ src/main/java/dev/slohth/rubikscube/RubiksCube.java
@@ -9,26 +9,9 @@ public final class RubiksCube {
 
     public void run() {
 
-        /*
-
-        2 is FRONT
-        all rotations are described relative to the FRONT
-        aka RIGHT, is moving side 2 to the RIGHT
-
-          0
-        1 2 3 4
-          5
-         (4)
-
-         */
-
         Cube cube = new Cube();
         cube.rotate(CubeRotation.RIGHT);
         cube.display();
 
-    }
-
-    private byte[] rotate(byte[] arr) {
-        return new byte[] { arr[6], arr[3], arr[0], arr[7], arr[4], arr[1], arr[8], arr[5], arr[2] };
     }
 }
