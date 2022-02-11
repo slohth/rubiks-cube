b999eac2f6ef019775bc5f55d12c80f3e9ca63f4
Slohth <hellspawnp@gmail.com>
2022-02-11 01:04:28 +0000

    Optimize imports

--- src/main/java/dev/slohth/rubikscube/display/Display.java
+++ src/main/java/dev/slohth/rubikscube/display/Display.java
@@ -8,7 +8,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
-import java.util.concurrent.FutureTask;
 
 public class Display implements ActionListener {
 
